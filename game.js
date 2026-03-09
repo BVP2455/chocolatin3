8480542164a4c6f448567d53d03edd0986b1d64e
Marlene <wykpvpd6pr@privaterelay.appleid.com>
2026-03-09 18:11:59 +0100

    Update game.js
    chages a little bit the document added how long the jump is

--- game.js
+++ game.js
@@ -3,10 +3,19 @@ velocityY += gravity;
 
 document.addEventListener("keydown", function(event) {
     if (event.code === "Space" && player.isOnGround) {
-        player.velocityY = -20; // Adjust jump hight)
+        player.velocityY = -15; // Adjust jump hight)
+        const gravity = 0.6; // gravity
         player.isOnGround = false;
+        player.velocityY += gravity; // gravity pulls down player after jump
+        player.y = player.velocityY; // speed player moves
     }
 
-if (!player.isOnGround) {
-    airtime += 1
+if (player.y >= groundY) { // >= since happens when player below ground level since top of screen 0 and towards buttom higher number y 
+    player.y = groundY; // reset player to ground level
+    player.velocityY = 0
+    player.isOnGround = true; 
+}
 
+    if (!player.isOnGround) {
+    airtime += 1
+    }
