2b75fd1b47912242bac42154199da0f45b21f406
linneabjh <lihel7262@uib.no>
2026-03-09 17:55:20 +0100

    changes for canvas

--- game.js
+++ game.js
@@ -1,3 +1,8 @@
+background = document.getElementById("background");
+context = background.getContext("2d");
+
+
+
 // Listen for when a key is pressed
 document.addEventListener("keydown", function(event) {
     if (event.code === "Space") {
