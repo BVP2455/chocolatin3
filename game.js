a826b0b55b374d107c4d7d5cc5ce2d814fde4c85
Marlene <wykpvpd6pr@privaterelay.appleid.com>
2026-03-09 17:59:51 +0100

    Update game.js

--- game.js
+++ game.js
@@ -1,19 +1,12 @@
-background = document.getElementById("background");
-context = background.getContext("2d");
-
-
-
-// Listen for when a key is pressed
-document.addEventListener("keydown", function(event) {
-    if (event.code === "Space") {
-        jump();
-    }
-}); 
-
 velocityY -= jumpForce;
 velocityY += gravity;
 
 document.addEventListener("keydown", function(event) {
     if (event.code === "Space" && player.isOnGround) {
         player.velocityY = -20; // Adjust jump hight)
+        player.isOnGround = false;
     }
+
+if (!player.isOnGround) {
+    airtime += 1
+
