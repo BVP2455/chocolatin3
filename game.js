d840e63e5863b88fd355f178cec8ab6138003674
Marlene <wykpvpd6pr@privaterelay.appleid.com>
2026-03-09 17:53:57 +0100

    Update game.js

--- game.js
+++ game.js
@@ -1,10 +1,7 @@
-// Listen for when a key is pressed
-document.addEventListener("keydown", function(event) {
-    if (event.code === "Space") {
-        jump();
-    }
-}); 
-
 velocityY -= jumpForce;
 velocityY += gravity;
 
+document.addEventListener("keydown", function(event) {
+    if (event.code === "Space" && player.isOnGround) {
+        player.velocityY = -20; // Adjust jump hight)
+    }
